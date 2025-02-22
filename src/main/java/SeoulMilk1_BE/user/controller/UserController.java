a03af8c94debf1253f3a61fad80a7eca373935a1
Amepistheo <kellylhji@gmail.com>
2025-02-22 21:57:47 +0900

    [INIT] #1 : User 관련 패키지 및 파일 생성

---
+++ src/main/java/SeoulMilk1_BE/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package SeoulMilk1_BE.user.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "User", description = "유저 관련 API")
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+}
