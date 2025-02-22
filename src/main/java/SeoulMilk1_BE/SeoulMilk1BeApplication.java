08ec8dab603879d7bc1c44000bb3519b45b1740b
m020202 <m020202@naver.com>
2025-02-22 15:06:27 +0900

    Initial Commit

---
+++ src/main/java/SeoulMilk1_BE/SeoulMilk1BeApplication.java
@@ -0,0 +1,13 @@
+package SeoulMilk1_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulMilk1BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulMilk1BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SeoulMilk1_BE/SeoulMilk1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package SeoulMilk1_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulMilk1BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
