57b6efd4ee769f19a454c24a3afd5d290545befb
m020202 <m020202@naver.com>
2025-02-25 21:12:11 +0900

    [FEAT] #9: 댓글 엔티티 추가

---
+++ src/main/java/SeoulMilk1_BE/post/domain/Comment.java
@@ -0,0 +1,27 @@
+package SeoulMilk1_BE.post.domain;
+
+import SeoulMilk1_BE.global.domain.BaseTimeEntity;
+import SeoulMilk1_BE.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String text;
+}
