99c796c88a29008edf05dd1001fd51cc5bca00e0
m020202 <m020202@naver.com>
2025-02-25 22:02:34 +0900

    [CHORE] #10: S3 관련 세팅

---
+++ src/main/java/SeoulMilk1_BE/global/service/S3Service.java
@@ -0,0 +1,113 @@
+package SeoulMilk1_BE.global.service;
+
+import SeoulMilk1_BE.global.apiPayload.code.status.ErrorStatus;
+import SeoulMilk1_BE.global.apiPayload.exception.GeneralException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final AmazonS3 amazonS3;
+
+    @Value("${spring.cloud.aws.s3.bucketName}")
+    private String bucketName;
+
+
+    // 이미지 단 건 업로드
+    public String uploadFile(MultipartFile file) {
+        validateFile(file);
+
+        String fileName = generateUniqueFileName(file.getOriginalFilename());
+        ObjectMetadata metadata = new ObjectMetadata();
+
+        pubImage(fileName, metadata, file);
+
+        // 업로드된 파일의 URL 반환
+        return amazonS3.getUrl(bucketName, fileName).toString();
+    }
+
+    // 이미지 다 건 업로드
+    public List<String> uploadFiles(List<MultipartFile> files) {
+        List<String> fileUrls = new ArrayList<>();
+        String fileName = "";
+        ObjectMetadata metadata = new ObjectMetadata();
+
+        for (MultipartFile file : files) {
+            validateFile(file);
+
+            fileName = generateUniqueFileName(file.getOriginalFilename());
+
+            pubImage(fileName, metadata, file);
+            fileUrls.add(amazonS3.getUrl(bucketName, fileName).toString());
+        }
+
+        return fileUrls;
+    }
+
+    // s3에 등록
+    private void pubImage(String fileName, ObjectMetadata metadata, MultipartFile file) {
+        try (InputStream inputStream = file.getInputStream()) {
+            metadata.setContentLength(file.getSize());
+            metadata.setContentType(file.getContentType());
+
+            amazonS3.putObject(new PutObjectRequest(bucketName, fileName, inputStream, metadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new GeneralException(ErrorStatus.FILE_NOT_UPLOADED);
+        }
+    }
+
+    private void validateFile(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new GeneralException(ErrorStatus.FILE_IS_EMPTY);
+        }
+
+        String contentType = file.getContentType();
+        if (contentType == null || !contentType.startsWith("image/")) {
+            throw new GeneralException(ErrorStatus.FILE_NOT_IMAGE);
+        }
+    }
+
+    private String generateUniqueFileName(String originalFilename) {
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uniqueId = UUID.randomUUID().toString().replace("-", "");
+        return uniqueId + extension;
+    }
+
+    public void deleteImageFromS3(String imageAddress){
+        String key = getKeyFromImageAddress(imageAddress);
+        try{
+            amazonS3.deleteObject(new DeleteObjectRequest(bucketName, key));
+        }catch (Exception e){
+            throw new GeneralException(ErrorStatus.IO_EXCEPTION_ON_IMAGE_DELETE);
+        }
+    }
+
+    private String getKeyFromImageAddress(String imageAddress){
+        try{
+            URL url = new URL(imageAddress);
+            String decodingKey = URLDecoder.decode(url.getPath(), "UTF-8");
+            return decodingKey.substring(1); // 맨 앞의 '/' 제거
+        } catch (MalformedURLException | UnsupportedEncodingException e){
+            throw new GeneralException(ErrorStatus.IO_EXCEPTION_ON_IMAGE_DELETE);
+        }
+    }
+}
