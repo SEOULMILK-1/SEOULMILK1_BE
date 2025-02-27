ba25f6ea279de1e86f502edcf25768e02bf8e759
Amepistheo <kellylhji@gmail.com>
2025-02-27 11:44:31 +0900

    [REFACTOR] #11: 승인 대기중인 회원 리스트 Response 값 수정

--- src/main/java/SeoulMilk1_BE/user/dto/response/PendingUserResponse.java
+++ src/main/java/SeoulMilk1_BE/user/dto/response/PendingUserResponse.java
@@ -4,26 +4,46 @@ import SeoulMilk1_BE.user.domain.User;
 import SeoulMilk1_BE.user.domain.type.Role;
 import SeoulMilk1_BE.user.domain.type.Team;
 import lombok.Builder;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Builder
 public record PendingUserResponse(
         Long userId,
         Long employeeId,
         String name,
-        String email,
         String phone,
         Role role,
-        Team team
+        Team team,
+        String createdAt
 ) {
     public static PendingUserResponse from(User user) {
         return PendingUserResponse.builder()
                 .userId(user.getId())
                 .employeeId(user.getEmployeeId())
                 .name(user.getName())
-                .email(user.getEmail())
-                .phone(user.getPhone())
+                .phone(formatPhone(user.getPhone()))
                 .role(user.getRole())
                 .team(user.getTeam())
+                .createdAt(formatCreatedAt(user.getCreatedAt()))
                 .build();
+    }
+
+    private static String formatPhone(String phone) {
+        if (StringUtils.hasText(phone) && phone.matches("\\d{10,11}")) {
+            return phone.replaceFirst("(\\d{3})(\\d{4})(\\d{4})", "$1-$2-$3");
+        }
+        return phone;
+    }
+
+    private static String formatCreatedAt(LocalDateTime createdAt) {
+        if (createdAt != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+            return createdAt.format(formatter);
+        }
+
+        return String.valueOf(createdAt);
     }
 }
