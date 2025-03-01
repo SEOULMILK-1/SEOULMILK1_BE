4dbf1918840a1f03fd3af09da0a18bef070db8f2
Amepistheo <kellylhji@gmail.com>
2025-03-01 16:19:53 +0900

    [REFACTOR] #18: 관리자 관련 코드 AdminService로 이동

--- src/main/java/SeoulMilk1_BE/user/service/UserService.java
+++ src/main/java/SeoulMilk1_BE/user/service/UserService.java
@@ -1,21 +1,13 @@
 package SeoulMilk1_BE.user.service;
 
 import SeoulMilk1_BE.user.domain.User;
-import SeoulMilk1_BE.user.dto.response.PendingUserResponse;
-import SeoulMilk1_BE.user.dto.response.PendingUserResponseList;
 import SeoulMilk1_BE.user.exception.UserNotFoundException;
 import SeoulMilk1_BE.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 import static SeoulMilk1_BE.global.apiPayload.code.status.ErrorStatus.USER_NOT_FOUND;
-import static SeoulMilk1_BE.user.util.UserConstants.*;
 
 @Service
 @RequiredArgsConstructor
@@ -24,32 +16,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public PendingUserResponseList findPendingUsers(int page, int size) {
-        Pageable pageable = PageRequest.of(page, size);
 
-        Page<User> userPage = userRepository.findUsersByRoleHQOrAgencyAndNotAssigned(pageable);
-
-        List<PendingUserResponse> responseList = userPage.stream()
-                .map(PendingUserResponse::from)
-                .toList();
-
-        return PendingUserResponseList.of(userPage, responseList);
-    }
-
-
-    @Transactional
-    public String approvePendingUser(Long userId) {
-        User user = findUser(userId);
-        user.assign();
-        return APPROVED.getMessage();
-    }
-
-    @Transactional
-    public String deleteUser(Long userId) {
-        User user = findUser(userId);
-        userRepository.delete(user);
-        return DELETED.getMessage();
-    }
 
     public User findUser(Long userId) {
         return userRepository.findById(userId)
