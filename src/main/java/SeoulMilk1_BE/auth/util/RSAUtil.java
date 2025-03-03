3ab802beb4c62effd84f169455694d1e70597a65
m020202 <m020202@naver.com>
2025-03-04 00:09:55 +0900

    [CHORE] #17: 암호화 관련 예외 처리 수정

--- src/main/java/SeoulMilk1_BE/auth/util/RSAUtil.java
+++ src/main/java/SeoulMilk1_BE/auth/util/RSAUtil.java
@@ -1,5 +1,7 @@
 package SeoulMilk1_BE.auth.util;
 
+import SeoulMilk1_BE.global.apiPayload.code.status.ErrorStatus;
+import SeoulMilk1_BE.global.apiPayload.exception.GeneralException;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
@@ -14,18 +16,22 @@ public class RSAUtil {
     /**
      * RSA 암호화
      **/
-    public String encryptRSA(String password, String publicKey) throws Exception{
-        PublicKey publicKeyFromString = getPublicKeyFromString(publicKey);
+    public String encryptRSA(String password, String publicKey){
+        try {
+            PublicKey publicKeyFromString = getPublicKeyFromString(publicKey);
 
-        // PEM 형식의 공개키 문자열을 Base64로 디코딩
-        // RSA 암호화 수행
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKeyFromString);  // 암호화 모드로 초기화
+            // PEM 형식의 공개키 문자열을 Base64로 디코딩
+            // RSA 암호화 수행
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKeyFromString);  // 암호화 모드로 초기화
 
-        byte[] encryptedPassword = cipher.doFinal(password.getBytes(StandardCharsets.UTF_8)); // 비밀번호 암호화
+            byte[] encryptedPassword = cipher.doFinal(password.getBytes(StandardCharsets.UTF_8)); // 비밀번호 암호화
 
-        // Base64로 인코딩하여 출력 (문자열로 안전하게 전달)
-        return Base64.getEncoder().encodeToString(encryptedPassword);
+            // Base64로 인코딩하여 출력 (문자열로 안전하게 전달)
+            return Base64.getEncoder().encodeToString(encryptedPassword);
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.ENCRYPTION_FAILED);
+        }
     }
 
     private PublicKey getPublicKeyFromString(String publicKey) throws Exception {
