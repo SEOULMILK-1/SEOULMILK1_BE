b01ecb2dd16543cedee77a0f9ed865200d0ba286
Amepistheo <kellylhji@gmail.com>
2025-03-05 20:48:42 +0900

    [FEAT] #25: 대리점 세금계산서 상세 조회 기능 구현

--- src/main/java/SeoulMilk1_BE/user/service/CsService.java
+++ src/main/java/SeoulMilk1_BE/user/service/CsService.java
@@ -1,8 +1,11 @@
 package SeoulMilk1_BE.user.service;
 
+import SeoulMilk1_BE.nts_tax.domain.NtsTax;
 import SeoulMilk1_BE.nts_tax.domain.type.ValidStatus;
 import SeoulMilk1_BE.nts_tax.dto.response.CsSearchTaxResponse;
 import SeoulMilk1_BE.nts_tax.dto.response.CsSearchTaxResponseList;
+import SeoulMilk1_BE.nts_tax.dto.response.CsTaxDetailResponse;
+import SeoulMilk1_BE.nts_tax.exception.NtsTaxNotFoundException;
 import SeoulMilk1_BE.nts_tax.repository.NtsTaxRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
+
+import static SeoulMilk1_BE.global.apiPayload.code.status.ErrorStatus.TAX_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
@@ -33,6 +38,13 @@ public class CsService {
         List<CsSearchTaxResponse> responseList = csTaxResponsePage.getContent();
 
         return CsSearchTaxResponseList.of(totalElements, totalPages, responseList);
+    }
+
+    public CsTaxDetailResponse getTaxDetail(Long ntsTaxId) {
+        NtsTax ntsTax = ntsTaxRepository.findById(ntsTaxId)
+                .orElseThrow(() -> new NtsTaxNotFoundException(TAX_NOT_FOUND));
+
+        return CsTaxDetailResponse.from(ntsTax);
     }
 
     private static String formatInputData(String inputData) {
