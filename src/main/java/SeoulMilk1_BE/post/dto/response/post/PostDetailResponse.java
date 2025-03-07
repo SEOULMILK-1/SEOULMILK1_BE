899f83103144c128fc7beb631f389c60a19ec1f9
m020202 <m020202@naver.com>
2025-03-07 18:06:16 +0900

    [REFACXTOR] #28: 메서드명 변경

--- src/main/java/SeoulMilk1_BE/post/dto/response/post/PostDetailResponse.java
+++ src/main/java/SeoulMilk1_BE/post/dto/response/post/PostDetailResponse.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record PostDetailResponse(Long postId, String userName, Role role, String title, String content, Long views, List<String> imgUrls, LocalDateTime updatedAt, List<CommentReadResponse> comments, Boolean pin) {
-    public static PostDetailResponse from(Long postId, String userName, Role role, String title, String content, Long views, List<String> imgUrls, LocalDateTime updatedAt, List<CommentReadResponse> comments, Boolean pin) {
+    public static PostDetailResponse of(Long postId, String userName, Role role, String title, String content, Long views, List<String> imgUrls, LocalDateTime updatedAt, List<CommentReadResponse> comments, Boolean pin) {
         return new PostDetailResponse(postId, userName, role, title, content, views, imgUrls, updatedAt, comments, pin);
     }
 }
