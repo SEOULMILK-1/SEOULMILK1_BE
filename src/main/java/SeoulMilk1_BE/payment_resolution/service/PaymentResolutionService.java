3dadffaf251a2a05777ce0e9a187c2fe6ccd8e05
m020202 <m020202@naver.com>
2025-03-09 15:32:39 +0900

    [FEAT] #38: 지급결의서 html -> pdf 파일 생성 로직 구현

--- src/main/java/SeoulMilk1_BE/payment_resolution/service/PaymentResolutionService.java
+++ src/main/java/SeoulMilk1_BE/payment_resolution/service/PaymentResolutionService.java
@@ -11,11 +11,23 @@ import SeoulMilk1_BE.payment_resolution.dto.response.PaymentResolutionReadRespon
 import SeoulMilk1_BE.payment_resolution.repository.PaymentResolutionRepository;
 import SeoulMilk1_BE.payment_resolution.utils.PaymentResolutionConstants;
 import SeoulMilk1_BE.user.service.TeamService;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.BaseFont;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -66,5 +78,68 @@ public class PaymentResolutionService {
         PageRequest pageRequest = PageRequest.of(page, size);
         List<PaymentResolution> result = paymentResolutionRepository.findListByDeadline(LocalDateTime.now().minusMonths(period), pageRequest).getContent();
         return result.stream().map(p -> PaymentResolutionListResponse.from(p)).collect(Collectors.toList());
+    }
+
+    private String parseThymeleafTemplate(PaymentResolution paymentResolution) {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        Context context = new Context();
+        context.setVariable("title", paymentResolution.getName());
+        context.setVariable("writeDate", paymentResolution.getModifiedAt());
+        context.setVariable("docNumber", "1234567890");
+        context.setVariable("recipient", "서울우유태평고객센터");
+        context.setVariable("payer", "서울우유태평고객센터");
+        context.setVariable("recipientBusinessNumber", "1029301924");
+        context.setVariable("payerBusinessNumber", "1029301924");
+        context.setVariable("totalAmount", "654,321,000");
+        context.setVariable("approver", "문정욱");
+        context.setVariable("paymentMethod", "계좌이체");
+        context.setVariable("approvalDate", "2025.02.26");
+        context.setVariable("bankAccount", "국민 1234567891234");
+        context.setVariable("scheduledDate", "2025.03.09");
+
+        return templateEngine.process("payment_resolution", context);
+    }
+
+    private ByteArrayOutputStream generatePdfFromHtml(String html) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.getFontResolver()
+                    .addFont(
+                            new ClassPathResource("/templates/NanumBarunGothic.ttf")
+                                    .getURL()
+                                    .toString(),
+                            BaseFont.IDENTITY_H,
+                            BaseFont.EMBEDDED);
+            renderer.setDocumentFromString(html);
+            renderer.layout();
+            renderer.createPDF(outputStream);
+
+            outputStream.close();
+            return outputStream;
+        } catch (Exception e) {
+            throw new GeneralException(ErrorStatus.FAILED_TO_PDF);
+        }
+    }
+
+    public byte[] createPdfFile(Long id) {
+        PaymentResolution paymentResolution = paymentResolutionRepository.findById(id).orElseThrow(() -> new GeneralException(ErrorStatus.PAYMENT_RESOLUTION_NOT_FOUND));
+
+        String paymentResolutionHtml = parseThymeleafTemplate(paymentResolution);
+        ByteArrayOutputStream pdf = generatePdfFromHtml(paymentResolutionHtml);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("filename", paymentResolution.getName() + ".pdf");
+
+        return pdf.toByteArray();
     }
 }
