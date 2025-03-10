ec236543d7f64f69e579ce76cf7d13ba008c20e2
m020202 <m020202@naver.com>
2025-03-10 21:01:34 +0900

    [REFACTOR] #41: 지급결의서 상세 조회 시 세금계산서 ID도 출력되도록 변경

--- src/main/java/SeoulMilk1_BE/payment_resolution/domain/PaymentDetails.java
+++ src/main/java/SeoulMilk1_BE/payment_resolution/domain/PaymentDetails.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class PaymentDetails {
+    private Long ntsId;
     private String ntsTaxNum;
     private Long supplyAmount;
     private LocalDateTime issueDate;
