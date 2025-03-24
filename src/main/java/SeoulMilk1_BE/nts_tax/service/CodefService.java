352815438d97fb6de863a4cea86f82963ccb9e72
m020202 <m020202@naver.com>
2025-03-24 20:02:51 +0900

    [REFACTOR] #71: 불필요한 주석 및 의존성 제거

--- src/main/java/SeoulMilk1_BE/nts_tax/service/CodefService.java
+++ src/main/java/SeoulMilk1_BE/nts_tax/service/CodefService.java
@@ -1,31 +1,20 @@
 package SeoulMilk1_BE.nts_tax.service;
 
-import SeoulMilk1_BE.auth.util.RSAUtil;
 import SeoulMilk1_BE.nts_tax.dto.request.CodefApiRequest;
 import SeoulMilk1_BE.nts_tax.dto.request.CodefValidateRequest;
 import SeoulMilk1_BE.nts_tax.util.CodefConfigProperties;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.codef.api.EasyCodefConstant;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.codec.binary.Base64;
-import org.json.JSONObject;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -56,109 +45,6 @@ public class CodefService extends EasyCodefConstant {
                 .body(HashMap.class)
                 .get("access_token");
     }
-
-//    /**
-//     * 기본 요청 값 세팅
-//     **/
-//    private JSONObject setRequestJsonObject(CodefApiRequest request){
-//        JSONObject jsonRequest = new JSONObject();
-//        jsonRequest.put("organization", "0004");
-//
-//        // 사용자 관련
-//        jsonRequest.put("loginType", "0");
-//        jsonRequest.put("certType", "pfx");
-//        jsonRequest.put("certFile", properties.getClient_pfx());
-//        jsonRequest.put("certPassword", rsaUtil.encryptRSA(properties.getClient_password(), properties.getClient_publickey()));
-//        jsonRequest.put("supplierRegNumber", request.suId());
-//        jsonRequest.put("contractorRegNumber", request.ipId());
-//        jsonRequest.put("approvalNo", request.issueId());
-//        jsonRequest.put("reportingDate", request.issueDate());
-//        jsonRequest.put("supplyValue", request.chargeTotal());
-//
-//        return jsonRequest;
-//    }
-//
-//    /**
-//     * 요청 헤더 세팅
-//     **/
-//    private static HttpURLConnection getHttpURLConnection(String accessToken, URL url) throws IOException {
-//        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-//        con.setRequestMethod("POST");
-//        con.setRequestProperty("Content-Type", "application/json");
-//        con.setRequestProperty("Authorization", "Bearer " + accessToken);
-//        con.setDoOutput(true);
-//        return con;
-//    }
-//
-//    /**
-//     * API 요청
-//     **/
-//    private String getIssuedTaxInvoiceInfo(String accessToken, CodefApiRequest request) {
-//        BufferedReader br = null;
-//        try {
-//            URL url = new URL(properties.getClient_url());
-//            HttpURLConnection con = getHttpURLConnection(accessToken, url);
-//
-//
-//            JSONObject jsonRequest = setRequestJsonObject(request);
-//
-//            byte[] jsonData = jsonRequest.toString().getBytes(StandardCharsets.UTF_8);
-//
-//            try (OutputStream os = con.getOutputStream()) {
-//                os.write(jsonData);
-//            }
-//
-//            // 응답 코드 확인
-//            int responseCode = con.getResponseCode();
-//            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 응답
-//                br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
-//            } else { // 오류 발생 시
-//                br = new BufferedReader(new InputStreamReader(con.getErrorStream(), "utf-8"));
-//            }
-//
-//            // 응답 데이터 읽기
-//            StringBuilder response = new StringBuilder();
-//            String inputLine;
-//            while ((inputLine = br.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//
-//            return response.toString();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return null;
-//        } finally {
-//            if (br != null) {
-//                try {
-//                    br.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//
-//                }
-//            }
-//        }
-//    }
-//
-//    public String validateNtsTax(CodefApiRequest request) {
-//        String accessToken = publishToken();
-//        String result = getIssuedTaxInvoiceInfo(accessToken, request);
-//        return extractResult(result);
-//    }
-//
-//    /**
-//     * 결과 값 디코딩
-//     **/
-//    private String extractResult(String result) {
-//        try {
-//            String decode = URLDecoder.decode(result, StandardCharsets.UTF_8);
-//            ObjectMapper mapper = new ObjectMapper();
-//            JsonNode jsonNode = mapper.readTree(decode);
-//            return jsonNode.path("result").path("message").asText();
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 
     public Mono<String> getValidationResult(CodefApiRequest request) {
         String accessToken = "Bearer " + publishToken();
