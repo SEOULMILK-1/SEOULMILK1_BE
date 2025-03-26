22fd735c4a48293f42e6b7539f9f2281290ab18d
m020202 <m020202@naver.com>
2025-03-26 11:16:08 +0900

    [CHORE] #75: Lettuce 클라이언트로 변경

--- src/main/java/SeoulMilk1_BE/global/config/RedisConfig.java
+++ src/main/java/SeoulMilk1_BE/global/config/RedisConfig.java
@@ -6,6 +6,11 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -13,12 +18,18 @@ public class RedisConfig {
     private String host;
     @Value("%{spring.data.redis.port}")
     private int port;
-    private static final String REDISSON_HOST_PREFIX = "redis://";
 
     @Bean
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        config.useSingleServer().setAddress(REDISSON_HOST_PREFIX + host + ":" + port);
-        return Redisson.create(config);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration(host, port));
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
     }
 }
