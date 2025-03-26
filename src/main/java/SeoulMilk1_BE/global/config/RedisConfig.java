749ccd9cb1baa7c1035d506f0c67a272a83fefd7
m020202 <m020202@naver.com>
2025-03-26 11:23:23 +0900

    [FIX] #75: port @value 매핑 수정

--- src/main/java/SeoulMilk1_BE/global/config/RedisConfig.java
+++ src/main/java/SeoulMilk1_BE/global/config/RedisConfig.java
@@ -1,8 +1,5 @@
 package SeoulMilk1_BE.global.config;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +13,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
     private String host;
-    @Value("%{spring.data.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
